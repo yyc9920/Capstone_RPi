4bc8f3e6381ced0a7b1010e6e901deaef40b6a13
Yechan Yun <yyc837021@gmail.com>
2020-06-30 14:14:12 +0900

    Readme & Comments

--- Shell_Script/start_servo.sh
+++ Shell_Script/start_servo.sh
@@ -4,8 +4,9 @@ bluetoothctl discoverable on
 while :
 do
 	sudo rfcomm watch hci0 dp12cks34 &
-	# Waiting for bluetooth device to be connected
+	# Waiting for bluetooth device to be connected in background
 	# dp12cks34 => chage this to your RPi password
 
 	python3 /home/pi/Capstone_design/Tracker_hor/multiple_tracker_servo_hor.py
+	# Track face with servo motor
 done
