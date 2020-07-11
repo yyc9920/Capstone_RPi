079f6f2d21d324dc9a498aa58c455d78a34d5299
yyc9920 <yyc837021@gmail.com>
2020-07-11 17:58:24 +0900

    Solving Issue#1 2

--- Tracker_hor/read_coord.py
+++ Tracker_hor/read_coord.py
@@ -9,29 +9,32 @@ b_device = Path("/dev/rfcomm0")
 exception_flag = 0
 
 def getCoord():
-	while True:
-            if(b_device.exists() == True):
-                    # Read Coordinates String via Bluetooth Communication
-                    try:
-                            ser = serial.Serial('/dev/rfcomm0')
-                            coord = ser.readline()
-                            test = ser.readline()
-                            coord = coord.decode('utf-8')
-                            coord = coord[0:-2]
-                    except serial.serialutil.SerialException as e:
-                            print(e)
-                            print("Bluetooth connection lost")
-                            exception_flag = 1
-                            break
-                    else:
-                            pass
+    while True:
+        global b_device
+        global exception_flag
+        if(b_device.exists() == True):
+            # Read Coordinates String via Bluetooth Communication
+            try:
+                ser = serial.Serial('/dev/rfcomm0')
+                coord = ser.readline()
+                coord = coord.decode('utf-8')
+                coord = coord[0:-2]
+                test = ser.readlines(5)
+                #test = test.decode('utf-8')
+            except serial.serialutil.SerialException as e:
+                print(e)
+                print("Bluetooth connection lost")
+                exception_flag = 1
+                break
             else:
-                    continue
+                pass
+        else:
+            continue
 
-            print("coord :")
-            print(coord)
-            print("test")
-            print(test)
+        print("coord :")
+        print(coord)
+        print("test :")
+        print(test)
 
 while True:
     if(b_device.exists() == True):
