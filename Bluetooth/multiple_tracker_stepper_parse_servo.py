9190ec0ebb33bfdb0ba15e6cfb3b884adb56453f
yyc9920 <yyc837021@gmail.com>
2020-07-30 01:58:31 +0900

    ***최적 구도 기능 구현 진행현황***

--- Bluetooth/multiple_tracker_stepper_parse_servo.py
+++ Bluetooth/multiple_tracker_stepper_parse_servo.py
@@ -5,8 +5,8 @@ import re
 import pigpio
 from pathlib import Path
 
-servo_x = pigpio.pi() # Note 22 is pin, 100 = 100Hz pulse
-servo_y = pigpio.pi() # Note 22 is pin, 100 = 100Hz pulse
+servo_x = pigpio.pi() # gpio 25
+servo_y = pigpio.pi() # gpio 17
 
 b_device = Path("/dev/rfcomm0")
 
@@ -16,8 +16,8 @@ faceInfo = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 cnt = 0
 fin_x = 0
 fin_y = 0
-crd = [0, 0]
-tmp = [0, 0]
+crd = 0
+tmp = 0
 tmp2 = [0, 0]
 exception_flag = 0
 faceId = 0
@@ -31,8 +31,13 @@ time.sleep(2)
 def readSerialLine():
     while(1):
         global exception_flag
+        global servo_x
         global fin_x
         global fin_y
+        global duty_x
+        global duty_y
+        global tmp
+        global crd
         global coord
         global faceNum
         if(b_device.exists() == True):
@@ -47,81 +52,74 @@ def readSerialLine():
             except serial.serialutil.SerialException:
                 fin_x = 500
                 fin_y = 350
+                duty_x = 1475
+                servo_x.set_servo_pulsewidth(25, 1475)
                 print("Bluetooth connection lost")
                 exception_flag = 1
                 time.sleep(0.5)
                 break
         else:
             fin_x = 500
-            fin_y = 500
+            fin_y = 350
             continue
 
-        fn = int(faceNum[1])
+        tmp = faceNum[0].split('!')
+        crd = tmp[1].split('/')
         # crd#[0] => crd_x, crd#[1] => crd_y
-        if(fn == 1):
-            fi1 = faceNum[0][0:-1]
-            crd1 = fi1.split('/')
-            print('\033[36m' + "faces = 1" + '\033[0m')
-            print(crd1)
-            fin_x = float(crd1[0])
-            fin_y = float(crd1[1])
-        elif(fn == 2):
-            fi2 = re.split('!', faceNum[0][0:-1])
-            crd1 = fi2[0].split('/')
-            crd2 = fi2[1].split('/')
-            print('\033[36m' + "faces = 2" + '\033[0m')
-            print(crd1)
-            print(crd2)
-            fin_x = (float(crd1[0]) + float(crd2[0]))/2
-            fin_y = (float(crd1[1]) + float(crd2[1]))/2
-        elif(fn == 3):
-            fi3 = re.split('!|"', faceNum[0][0:-1])
-            crd1 = fi3[0].split('/')
-            crd2 = fi3[1].split('/')
-            crd3 = fi3[2].split('/')
-            print('\033[36m' + "faces = 3" + '\033[0m')
-            print(crd1)
-            print(crd2)
-            print(crd3)
-            fin_x = (float(crd1[0]) + float(crd2[0]) + float(crd3[0]))/3
-            fin_y = (float(crd1[1]) + float(crd2[1]) + float(crd3[1]))/3
-        elif(fn == 4):
-            fi4 = re.split('!|"|#', faceNum[0][0:-1])
-            crd1 = fi4[0].split('/')
-            crd2 = fi4[1].split('/')
-            crd3 = fi4[2].split('/')
-            crd4 = fi4[3].split('/')
-            print('\033[36m' + "faces = 4" + '\033[0m')
-            print(crd1)
-            print(crd2)
-            print(crd3)
-            print(crd4)
-            fin_x = (float(crd1[0]) + float(crd2[0]) + float(crd3[0]) + float(crd4[0]))/4
-            fin_y = (float(crd1[1]) + float(crd2[1]) + float(crd3[1]) + float(crd4[1]))/4
-        elif(fn == 5):
-            fi5 = re.split('!|"|#', faceNum[0][0:-1])
-            fi_tmp = fi5[3].split('$')
-            crd1 = fi5[0].split('/')
-            crd2 = fi5[1].split('/')
-            crd3 = fi5[2].split('/')
-            crd4 = fi_tmp[0].split('/')
-            crd5 = fi_tmp[1].split('/')
-            print('\033[36m' + "faces = 5" + '\033[0m')
-            print(crd1)
-            print(crd2)
-            print(crd3)
-            print(crd4)
-            print(crd5)
-            fin_x = (float(crd1[0]) + float(crd2[0]) + float(crd3[0]) + float(crd4[0]) + float(crd5[0]))/5
-            fin_y = (float(crd1[1]) + float(crd2[1]) + float(crd3[1]) + float(crd4[1]) + float(crd5[1]))/5
-        else:
-            fin_x = 500
-            fin_y = 350
-        print('\033[91m' + 'fin_x = ', end='')
-        print(fin_x, end='')
-        print(', fin_y = ', end='')
-        print(fin_y, end='')
-        print('\033[0m')
+        fin_x = float(crd[0])
+        fin_y = float(crd[1])
+        print('\033[92m' + "Mode : ", end='')
+        print(tmp[0] + '\033[0m')
+        print('\033[96m' + "x : ", end='')
+        print(crd[0])
+        print("y : ", end='')
+        print(crd[1] + '\033[0m')
+
+        if(tmp[0] == 'N' or tmp[0] == 'C'):
+            manx_rspeed = (fin_x - 550)/10
+            manx_lspeed = (fin_x - 450)/10
+            # Run Servo Motor to Right
+            if(fin_x > 550):
+                servo_x.set_servo_pulsewidth(25, duty_x)
+                duty_x = 1475 + manx_rspeed
+                time.sleep(0.03)
+            # Run Servo Motor to Left
+            elif(fin_x < 450):
+                servo_x.set_servo_pulsewidth(25, duty_x)
+                duty_x = 1475 + manx_lspeed
+                time.sleep(0.03)
+            else:
+                servo_x.set_servo_pulsewidth(25, 1475)
+        elif(tmp[0] == 'L'):
+            manx_rspeed = (fin_x - 384)/10
+            manx_lspeed = (fin_x - 283)/5
+            # Run Servo Motor to Right
+            if(fin_x > 384):
+                servo_x.set_servo_pulsewidth(25, duty_x)
+                duty_x = 1475 + manx_rspeed
+                time.sleep(0.03)
+            # Run Servo Motor to Left
+            elif(fin_x < 283):
+                servo_x.set_servo_pulsewidth(25, duty_x)
+                duty_x = 1475 + manx_lspeed
+                time.sleep(0.03)
+            else:
+                servo_x.set_servo_pulsewidth(25, 1475)
+        elif(tmp[0] == 'R'):
+            manx_rspeed = (fin_x - 717)/5
+            manx_lspeed = (fin_x - 616)/10
+            # Run Servo Motor to Right
+            if(fin_x > 717):
+                servo_x.set_servo_pulsewidth(25, duty_x)
+                duty_x = 1475 + manx_rspeed
+                time.sleep(0.03)
+            # Run Servo Motor to Left
+            elif(fin_x < 616):
+                servo_x.set_servo_pulsewidth(25, duty_x)
+                duty_x = 1475 + manx_lspeed
+                time.sleep(0.03)
+            else:
+                servo_x.set_servo_pulsewidth(25, 1475)
 
 def runServo_x():
     while True:
@@ -131,9 +129,10 @@ def runServo_x():
         global exception_flag
         global duty_x
         global duty_y
+        global tmp
+        global faceNum
 
-        manx_rspeed = (fin_x - 550)/10
-        manx_lspeed = (fin_x - 450)/10
+        tmp = faceNum[0].split('!')
 
         if(exception_flag == 1):
             print("test")
@@ -143,18 +142,51 @@ def runServo_x():
             time.sleep(0.5)
             break
 
-        # Run Servo Motor to Right
-        if(fin_x > 550):
-            servo_x.set_servo_pulsewidth(25, duty_x)
-            duty_x = 1475 + manx_rspeed
-            time.sleep(0.03)
-        # Run Servo Motor to Left
-        elif(fin_x < 450):
-            servo_x.set_servo_pulsewidth(25, duty_x)
-            duty_x = 1475 + manx_lspeed
-            time.sleep(0.03)
-        else:
-            servo_x.set_servo_pulsewidth(25, 1475)
+        if(tmp[0] == 'N' or tmp[0] == 'C'):
+            manx_rspeed = (fin_x - 550)/10
+            manx_lspeed = (fin_x - 450)/10
+            # Run Servo Motor to Right
+            if(fin_x > 550):
+                servo_x.set_servo_pulsewidth(25, duty_x)
+                duty_x = 1475 + manx_rspeed
+                time.sleep(0.03)
+            # Run Servo Motor to Left
+            elif(fin_x < 450):
+                servo_x.set_servo_pulsewidth(25, duty_x)
+                duty_x = 1475 + manx_lspeed
+                time.sleep(0.03)
+            else:
+                servo_x.set_servo_pulsewidth(25, 1475)
+        elif(tmp[0] == 'L'):
+            manx_rspeed = (fin_x - 384)/10
+            manx_lspeed = (fin_x - 283)/5
+            # Run Servo Motor to Right
+            if(fin_x > 384):
+                servo_x.set_servo_pulsewidth(25, duty_x)
+                duty_x = 1475 + manx_rspeed
+                time.sleep(0.03)
+            # Run Servo Motor to Left
+            elif(fin_x < 283):
+                servo_x.set_servo_pulsewidth(25, duty_x)
+                duty_x = 1475 + manx_lspeed
+                time.sleep(0.03)
+            else:
+                servo_x.set_servo_pulsewidth(25, 1475)
+        elif(tmp[0] == 'R'):
+            manx_rspeed = (fin_x - 717)/5
+            manx_lspeed = (fin_x - 616)/10
+            # Run Servo Motor to Right
+            if(fin_x > 717):
+                servo_x.set_servo_pulsewidth(25, duty_x)
+                duty_x = 1475 + manx_rspeed
+                time.sleep(0.03)
+            # Run Servo Motor to Left
+            elif(fin_x < 616):
+                servo_x.set_servo_pulsewidth(25, duty_x)
+                duty_x = 1475 + manx_lspeed
+                time.sleep(0.03)
+            else:
+                servo_x.set_servo_pulsewidth(25, 1475)
 
 def runServo_y():
     while True:
@@ -201,8 +233,7 @@ def runServo_y_thread():
 while(1):
     if(b_device.exists() == True):
         readSerialLine_thread()
-        runServo_y_thread()
-        runServo_x()
+        runServo_y()
     else:
         print("Bluetooth not connected")
         time.sleep(5)
