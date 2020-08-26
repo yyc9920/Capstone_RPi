94244a10637e02e3ca845091daa35becc6c61989
yyc9920 <yyc837021@gmail.com>
2020-08-26 13:34:51 +0900

    Rail mode demo

--- Bluetooth/phodo_final.py
+++ Bluetooth/phodo_final.py
@@ -6,7 +6,6 @@ import pigpio
 import RPi.GPIO as GPIO
 from pathlib import Path
 
-servo_x = pigpio.pi() # gpio 25
 servo_y = pigpio.pi() # gpio 25
 
 b_device = Path("/dev/rfcomm0")
@@ -14,22 +13,16 @@ b_device = Path("/dev/rfcomm0")
 fin_x = 0
 fin_y = 0
 crd = 0
-tmp = 0
+tmp = [0, 0]
 exception_flag = 0
 bSerData = 0
 bdataInfo = [0, 'S']
 duty_x = 1470 # halt duty cycle
 duty_y = 2100 # Perpendicular duty cycle
-servo_y.set_servo_pulsewidth(25, 2100)
+servo_y.set_servo_pulsewidth(25, 1470)
 time.sleep(2)
 
-GPIO.setmode(GPIO.BOARD)
-
 ControlPin = [7, 11, 13, 15]
-
-for pin in ControlPin:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin,0)
 
 seq = [ [1, 0, 0, 0],
         [1, 1, 0, 0],
@@ -43,7 +36,7 @@ seq = [ [1, 0, 0, 0],
 def readSerialLine():
     while(1):
         global exception_flag
-        global servo_x
+        global servo_y
         global fin_x
         global fin_y
         global duty_x
@@ -63,17 +56,17 @@ def readSerialLine():
                 fin_x = 500
                 fin_y = 350
                 duty_x = 1470
-                servo_x.set_servo_pulsewidth(25, 1470)
+                servo_y.set_servo_pulsewidth(25, 1470)
                 print('\033[31m' + "Bluetooth connection lost" + '\033[0m')
                 exception_flag = 1
                 time.sleep(0.5)
                 break
         else:
             fin_x = 500
-            servo_x.set_servo_pulsewidth(25, 1470)
+            servo_y.set_servo_pulsewidth(25, 1470)
 
         if(bSerData == 'E'):
-            servo_x.set_servo_pulsewidth(25, 1470)
+            servo_y.set_servo_pulsewidth(25, 1470)
             continue
         bdataInfo = bSerData.split(':')
         # bdataInfo[0] => composition mode + crd info, bdataInfo[1] => distance mode
@@ -93,23 +86,36 @@ def readSerialLine():
         print("y : ", end='')
         print(crd[1] + '\033[0m')
 
+def runStepper_x():
+    while True:
+        global tmp
+        global halfstep
+        global pin
+        global ControlPin
+        global seq
+        global fin_x
+
+        GPIO.setmode(GPIO.BOARD)
+
+        for pin in ControlPin:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin,0)
+
         if(tmp[0] == 'N' or tmp[0] == 'C'):
             manx_rspeed = (fin_x - 550)/400000
             manx_lspeed = (fin_x - 450)/400000
             # Run Stepper Motor to Right
             if(fin_x > 550):
-                for i in range(512):
-                    for halfstep in range(8):
-                        for pin in range(4):
-                            GPIO.output(ControlPin[pin], seq[halfstep][pin])
-                        time.sleep(0.0030-manx_rspeed)
+                for halfstep in range(8):
+                    for pin in range(4):
+                        GPIO.output(ControlPin[pin], seq[halfstep][pin])
+                    time.sleep(0.0030-manx_rspeed)
             # Run Stepper Motor to Left
             elif(fin_x < 450):
-                for i in range(512):
-                    for halfstep in range(8):
-                        for pin in range(4):
-                            GPIO.output(ControlPin[7-pin], seq[halfstep][pin])
-                        time.sleep(0.0030-manx_lspeed)
+                for halfstep in range(8):
+                    for pin in range(4):
+                        GPIO.output(ControlPin[3-pin], seq[halfstep][pin])
+                    time.sleep(0.0030-manx_lspeed)
             else:
                 pass
         elif(tmp[0] == 'L'):
@@ -117,18 +123,16 @@ def readSerialLine():
             manx_lspeed = (fin_x - 283)/250000
             # Run Stepper Motor to Right
             if(fin_x > 384):
-                for i in range(512):
-                    for halfstep in range(8):
-                        for pin in range(4):
-                            GPIO.output(ControlPin[pin], seq[halfstep][pin])
-                        time.sleep(0.0030-manx_rspeed)
+                for halfstep in range(8):
+                    for pin in range(4):
+                        GPIO.output(ControlPin[pin], seq[halfstep][pin])
+                    time.sleep(0.0030-manx_rspeed)
             # Run Stepper Motor to Left
             elif(fin_x < 283):
-                for i in range(512):
-                    for halfstep in range(8):
-                        for pin in range(4):
-                            GPIO.output(ControlPin[7-pin], seq[halfstep][pin])
-                        time.sleep(0.0030-manx_lspeed)
+                for halfstep in range(8):
+                    for pin in range(4):
+                        GPIO.output(ControlPin[3-pin], seq[halfstep][pin])
+                    time.sleep(0.0030-manx_lspeed)
             else:
                 pass
         elif(tmp[0] == 'R'):
@@ -136,18 +140,16 @@ def readSerialLine():
             manx_lspeed = (fin_x - 616)/550000
             # Run Servo Motor to Right
             if(fin_x > 717):
-                for i in range(512):
-                    for halfstep in range(8):
-                        for pin in range(4):
-                            GPIO.output(ControlPin[pin], seq[halfstep][pin])
-                        time.sleep(0.0030-manx_rspeed)
+                for halfstep in range(8):
+                    for pin in range(4):
+                        GPIO.output(ControlPin[pin], seq[halfstep][pin])
+                    time.sleep(0.0030-manx_rspeed)
             # Run Servo Motor to Left
             elif(fin_x < 616):
-                for i in range(512):
-                    for halfstep in range(8):
-                        for pin in range(4):
-                            GPIO.output(ControlPin[7-pin], seq[halfstep][pin])
-                        time.sleep(0.0030-manx_lspeed)
+                for halfstep in range(8):
+                    for pin in range(4):
+                        GPIO.output(ControlPin[3-pin], seq[halfstep][pin])
+                    time.sleep(0.0030-manx_lspeed)
             else:
                 pass
 
@@ -275,15 +277,17 @@ def readSerialLine_thread():
     thread.daemon = True
     thread.start()
 
-def runServo_y_thread():
-    thread = threading.Thread(target = runServo_y)
+def runStepper_x_thread():
+    thread = threading.Thread(target = runStepper_x)
     thread.daemon = True
     thread.start()
 
 while(1):
     if(b_device.exists() == True):
         readSerialLine_thread()
+        runStepper_x_thread()
         runServo_y()
     else:
+        GPIO.cleanup()
         print("Bluetooth not connected")
         time.sleep(5)
