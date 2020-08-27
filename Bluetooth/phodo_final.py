c0e8df70cea6f4013994ee79de4b4092e6d25aec
yyc9920 <yyc837021@gmail.com>
2020-08-27 17:45:26 +0900

    PHODO Final - 2

--- Bluetooth/phodo_final.py
+++ Bluetooth/phodo_final.py
@@ -4,7 +4,13 @@ import time
 import re
 import pigpio
 import RPi.GPIO as GPIO
+import sys
+import signal
 from pathlib import Path
+
+#GPIO 핀
+TRIG = 16 # 트리거
+ECHO = 18 # 에코
 
 servo_y = pigpio.pi() # gpio 25
 
@@ -17,8 +23,7 @@ tmp = [0, 0]
 exception_flag = 0
 bSerData = 'E'
 bdataInfo = [0, 'S']
-duty_x = 1470 # halt duty cycle
-duty_y = 2100 # Perpendicular duty cycle
+duty_y = 1470 # Perpendicular duty cycle
 servo_y.set_servo_pulsewidth(25, 1470)
 time.sleep(2)
 
@@ -34,13 +39,48 @@ seq = [ [1, 0, 0, 0],
 	[0, 0, 0, 1],
 	[1, 0, 0, 1] ]
 
+#거리 타임 아웃 용
+MAX_DISTANCE_CM = 300
+MAX_DURATION_TIMEOUT = (MAX_DISTANCE_CM * 2 * 29.1) #17460 # 17460us = 300cm
+
+distance = 0
+
+# 키보드 CTRL + C 누르면 종료 되게 처리
+def signal_handler(signal, frame):
+    print('You pressed Ctrl+C!')
+    GPIO.cleanup()
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+# cm 환산 함수
+# 아두이노 UltraDistSensor 코드에서 가져옴
+def distanceInCm(duration):
+    # 물체에 도착후 돌아오는 시간 계산
+    # 시간 = cm / 음속 * 왕복
+    # t   = 0.01/340 * 2= 0.000058824초 (58.824us)
+
+    # 인식까지의 시간
+    # t = 0.01/340 = 0.000029412초 (29.412us)
+
+    # duration은 왕복 시간이니 인식까지의 시간에서 2로 나눔
+    return (duration/2)/29.1
+
+
+# 거리 표시
+def print_distance(distance):
+    if distance == 0:
+        distanceMsg = 'Distance : out of range                   \r'
+    else:
+        distanceMsg = 'Distance : ' + str(distance) + 'cm' + '        \r'
+    sys.stdout.write(distanceMsg)
+    sys.stdout.flush()
+
 def readSerialLine():
     while(1):
         global exception_flag
         global servo_y
         global fin_x
         global fin_y
-        global duty_x
         global duty_y
         global tmp
         global crd
@@ -56,7 +96,6 @@ def readSerialLine():
             except serial.serialutil.SerialException:
                 fin_x = 500
                 fin_y = 350
-                duty_x = 1470
                 servo_y.set_servo_pulsewidth(25, 1470)
                 print('\033[31m' + "Bluetooth connection lost" + '\033[0m')
                 exception_flag = 1
@@ -96,6 +135,12 @@ def runStepper_x():
         global ControlPin_Rail
         global seq
         global fin_x
+        global distance
+
+        if(distance > 28.4):
+            distance = 0
+        elif(distance < 4):
+            distance = 0
 
         GPIO.setmode(GPIO.BOARD)
 
@@ -165,16 +210,23 @@ def runStepper_x():
                 manx_lspeed = (fin_x - 450)/400000
                 # Run Stepper Motor to Right
                 if(fin_x > 550):
-                    for halfstep in range(8):
-                        for pin in range(4):
-                            GPIO.output(ControlPin_Rail[pin], seq[halfstep][pin])
-                        time.sleep(0.0027-manx_rspeed)
+                    if(distance > 4.4):
+                        for halfstep in range(8):
+                            for pin in range(4):
+                                GPIO.output(ControlPin_Rail[pin], seq[halfstep][pin])
+                            time.sleep(0.0027-manx_rspeed)
+                    elif(distance == 0):
+                        pass
+
                 # Run Stepper Motor to Left
                 elif(fin_x < 450):
-                    for halfstep in range(8):
-                        for pin in range(4):
-                            GPIO.output(ControlPin_Rail[3-pin], seq[halfstep][pin])
-                        time.sleep(0.0027-manx_lspeed)
+                    if(distance < 28.0):
+                        for halfstep in range(8):
+                            for pin in range(4):
+                                GPIO.output(ControlPin_Rail[3-pin], seq[halfstep][pin])
+                            time.sleep(0.0027-manx_lspeed)
+                    elif(distance == 0):
+                        pass
                 else:
                     pass
 
@@ -297,6 +349,82 @@ def runServo_y():
             else:
                 time.sleep(0.1)
 
+def ultrasonic():
+    # 파이썬 GPIO 모드
+    GPIO.setmode(GPIO.BOARD)
+
+    # 핀 설정
+    GPIO.setup(TRIG, GPIO.OUT) # 트리거 출력
+    GPIO.setup(ECHO, GPIO.IN)  # 에코 입력
+
+    print('To Exit, Press the CTRL+C Keys')
+
+    # HC-SR04 시작 전 잠시 대기
+    GPIO.output(TRIG, False)
+    print('Waiting For Sensor To Ready')
+    time.sleep(1) # 1초
+
+    pulse_start = 0
+    pulse_end = 0
+
+    #시작
+    print('Start!!')
+    while True:
+        global distance
+        #171206 중간에 통신 안되는 문제 개선용      
+        fail = False
+        time.sleep(0.1)
+        # 트리거를 10us 동안 High 했다가 Low로 함.
+        # sleep 0.00001 = 10us
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+
+        # ECHO로 신호가 들어 올때까지 대기
+        timeout = time.time()
+        while GPIO.input(ECHO) == 0:
+            #들어왔으면 시작 시간을 변수에 저장
+            pulse_start = time.time()
+            if ((pulse_start - timeout)*1000000) >= MAX_DURATION_TIMEOUT:
+                #171206 중간에 통신 안되는 문제 개선용        
+                #continue
+                fail = True
+                break
+
+        #171206 중간에 통신 안되는 문제 개선용        
+        if fail:
+            continue
+        #ECHO로 인식 종료 시점까지 대기
+        timeout = time.time()
+        while GPIO.input(ECHO) == 1:
+            #종료 시간 변수에 저장
+            pulse_end = time.time()
+            if ((pulse_end - pulse_start)*1000000) >= MAX_DURATION_TIMEOUT:
+                print_distance(0) 
+                #171206 중간에 통신 안되는 문제 개선용        
+                #continue
+                fail = True
+                break
+
+        #171206 중간에 통신 안되는 문제 개선용        
+        if fail:
+            continue
+
+        #인식 시작부터 종료까지의 차가 바로 거리 인식 시간
+        pulse_duration = (pulse_end - pulse_start) * 1000000
+
+        # 시간을 cm로 환산
+        distance = distanceInCm(pulse_duration)
+        #print(pulse_duration)
+        #print('')
+        # 자리수 반올림
+        distance = round(distance, 2)
+
+        #표시
+        print_distance(distance)
+
+    GPIO.cleanup()
+
 def readSerialLine_thread():
     thread = threading.Thread(target = readSerialLine)
     thread.daemon = True
@@ -307,9 +435,15 @@ def runStepper_x_thread():
     thread.daemon = True
     thread.start()
 
+def ultrasonic_thread():
+    thread = threading.Thread(target = ultrasonic)
+    thread.daemon = True
+    thread.start()
+
 while(1):
     if(b_device.exists() == True):
         readSerialLine_thread()
+        ultrasonic_thread()
         runStepper_x_thread()
         runServo_y()
     else:
